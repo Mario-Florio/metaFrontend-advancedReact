70ca3e5b59d8fa0e98a0008124660e86d6603d96
ketherball <121590283+ketherball@users.noreply.github.com>
2023-09-14 10:57:46 -0400

    feat: create general structure for nav component

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,7 +3,14 @@ function Nav() {
 
     return(
         <nav className="Nav">
-            nav
+            <ul>
+                <li><a href="https://github.com/Mario-Florio">Github</a></li>
+            </ul>
+            <ul>
+                <li><a href="">About</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
         </nav>
     );
 }
