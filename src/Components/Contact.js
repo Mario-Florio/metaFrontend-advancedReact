04d0ffbdc7c8b19620ac8c584b6b04d118e673df
ketherball <121590283+ketherball@users.noreply.github.com>
2023-09-14 13:48:55 -0400

    feat: create basic styling for 'Contact' component

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,7 +3,18 @@ function Contact() {
 
     return(
         <section className="Contact">
-            contact
+            <h3>Contact Me</h3>
+            <form onSubmit={e => e.preventDefault()}>
+                <label>Name</label>
+                <input type="text"/>
+                <label>Email Address</label>
+                <input type="text"/>
+                <label>Type of enquiry</label>
+                <input type=""/>
+                <label>Your message</label>
+                <textarea/>
+                <button>Submit</button>
+            </form>
         </section>
     );
 }
