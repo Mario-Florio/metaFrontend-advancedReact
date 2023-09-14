0e9a7485d2d838b69b67ae14da9f6d9907b520bb
ketherball <121590283+ketherball@users.noreply.github.com>
2023-09-14 14:56:27 -0400

    feat: add form functionality

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,19 +1,70 @@
+import { useState } from "react";
 
 function Contact() {
+    const [name, setName] = useState(null);
+    const [nameInput, setNameInput] = useState("");
+
+    const [email, setEmail] = useState(null);
+    const [emailInput, setEmailInput] = useState("");
+
+    const [msg, setMsg] = useState(null);
+    const [msgInput, setMsgInput] = useState("");
+
+    const handleNameChange = e => {
+        setNameInput(e.target.value);
+    }
+
+    const handleEmailChange = e => {
+        setEmailInput(e.target.value);
+    }
+
+    const handleMsgChange = e => {
+        setMsgInput(e.target.value);
+    }
+
+    const handleSubmit = () => {
+        setName(nameInput);
+        setEmail(emailInput);
+        setMsg(msgInput);
+        setNameInput("");
+        setEmailInput("");
+        setMsgInput("");
+    }
 
     return(
         <section className="Contact">
             <h3>Contact Me</h3>
             <form onSubmit={e => e.preventDefault()}>
-                <label>Name</label>
-                <input type="text"/>
-                <label>Email Address</label>
-                <input type="text"/>
-                <label>Type of enquiry</label>
-                <input type=""/>
-                <label>Your message</label>
-                <textarea/>
-                <button>Submit</button>
+                <label htmlFor="name" id="name">
+                    Name
+                </label>
+                <input 
+                    type="text" 
+                    name="name"
+                    id="name"
+                    value={nameInput} 
+                    onChange={handleNameChange}
+                />
+                <label htmlFor="email" id="email">
+                    Email Address
+                </label>
+                <input 
+                    type="text"
+                    name="email"
+                    id="email"
+                    value={emailInput}
+                    onChange={handleEmailChange}
+                    />
+                <label htmlFor="msg" id="msg">
+                    Your message
+                </label>
+                <textarea
+                    name="msg"
+                    id="msg"
+                    value={msgInput}
+                    onChange={handleMsgChange}
+                />
+                <button onClick={handleSubmit}>Submit</button>
             </form>
         </section>
     );
