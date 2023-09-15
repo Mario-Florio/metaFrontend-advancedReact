b8ae5ee38d9c44613c171fdc2879d2799c00871b
ketherball <121590283+ketherball@users.noreply.github.com>
2023-09-15 10:24:20 -0400

    feat: add dynamic visibility to nav depending on scrollY

--- src/App.js
+++ src/App.js
@@ -4,19 +4,11 @@ import Landing from './Components/Landing/Landing';
 import Projects from './Components/Projects/Projects';
 import Contact from './Components/Contact/Contact';
 
-// 1. Add "on scroll display" to navbar
-
-// 2. Add error handling to form (use react forms?)
-
-// 3. Add links to certain scroll points on page
-
 function App() {
 
   return (
     <div className="App">
-      <header>
-        <Nav/>
-      </header>
+      <Nav/>
       <main>
         <Landing/>
         <Projects/>
